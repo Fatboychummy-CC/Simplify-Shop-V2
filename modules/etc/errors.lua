3e486b23557b367db8516cb8b1963f5035810fce
Matthew W <fatboychummy@gmail.com>
2019-07-14 01:07:38 -0600

    add optional error level

--- modules/etc/errors.lua
+++ modules/etc/errors.lua
@@ -1,8 +1,8 @@
 local funcs = {}
 
-function funcs.create(arg, expect, actual)
+function funcs.create(arg, expect, actual, level)
   return "Bad argument #" .. tostring(arg) .. ", expected " .. tostring(expect)
-         .. ", got " .. type(actual), 2
+         .. ", got " .. type(actual), level or 2
 end
 
 return funcs
