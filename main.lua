35b90cbbc2e6ffacdaa0422d8819d43a6fa041f4
Matthew W <fatboychummy@gmail.com>
2019-07-14 18:32:12 -0600

    move mainMenu to a function

--- main.lua
+++ main.lua
@@ -8,23 +8,47 @@
 local menus = require("modules.text.menus")
 local build = 0
 
+local function mainMenu()
+  local menu = menus.newMenu()
+  menu.title = "Simplify Shop V2B" .. tostring(build)
+  menu:addMenuItem(
+    "Run",
+    "Run the shop.",
+    "Run the shop."
+  )
+  --
+  menu:addMenuItem(
+    "Update",
+    "No update available.",
+    "Updates the shop and reboots."
+  )
+  --
+  menu:addMenuItem(
+    "Add/Remove",
+    "Add/Remove shop item(s).",
+    "Use a helpful UI to add items to your shop."
+  )
+  --
+  --
+  menu:addMenuItem(
+    "Options",
+    "Edit shop config.",
+    "Open a menu which allows you to change core settings for the shop."
+  )
+  --
+  --
+  --
+  --
+  return menu:go(5)
+end
+
 
 
 local function main()
-  local mainMenu = menus.newMenu()
-  mainMenu.title = "Simplify Shop V2B" .. tostring(build)
-  mainMenu:addMenuItem("Run", "Run the shop.",
-                        "Run the shop.")
-  --
-  mainMenu:addMenuItem("Update", "No update available.", "Updates the shop and "
-                                                         .. "reboots.")
-  --
-  mainMenu:addMenuItem("Add", "Add shop item(s).",
-                       "Use a helpful UI to add items to your shop.")
-  --
-  mainMenu:addMenuItem("Remove", "Remove shop item(s).",
-                       "Use a helpful UI to remove items from your shop.")
-  mainMenu:go(5)
+  local selection = 0
+  repeat
+    selection = mainMenu()
+  until selection == 1
 end
 
 local ok, err = pcall(main)
