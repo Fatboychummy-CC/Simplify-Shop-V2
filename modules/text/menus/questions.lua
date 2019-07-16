a577d1c0be051ecd84ad89372b8e61775dc3a166
Matthew W <fatboychummy@gmail.com>
2019-07-15 19:39:12 -0600

    update errors, add colors

--- modules/text/menus/questions.lua
+++ modules/text/menus/questions.lua
@@ -4,11 +4,15 @@ local met = {}
 meta.__index = met
 met.__type = "menuObject"
 
-local ec = require(".simplify.modules.etc.errors").watch
+local e = require(".simplify.modules.etc.errors")
+local ew = e.watch
+local ec = e.create
 
-local function ask(question, tp)
-  ec(1, "string", question)
-  ec(2, "string", tp)
+local function ask(self, question, tp)
+  ew(1, "table", self)
+  if self.__type ~= "menuObject" then error()
+  ew(2, "string", question)
+  ew(3, "string", tp)
 
   local answer = ""
 
@@ -25,20 +29,22 @@ local function ask(question, tp)
   return ""
 end
 
-function met:addQuestion(question, tp)
-  ec(1, "string", question)
-  ec(2, "string", tp)
+function met:addQuestion(question, tp, longInformation)
+  ew(1, "string", question)
+  ew(2, "string", tp)
+  ew(3, "string", longInformation)
 
   table.insert(tmp.questions.q, question)
   table.insert(tmp.questions.t, tp)
   table.insert(tmp.questions.a, "")
+  table.insert(tmp.questions.i, longInformation)
 
   return self
 end
 
 function met:go()
   for i, question in ipairs(self.questions.q) do
-    self.questions.a[i] = ask()
+    self.questions.a[i] = ask(self, question, self.questions.t[i])
   end
 end
 
@@ -50,6 +56,18 @@ function funcs.new()
     q = {},
     t = {},
     a = {}
+  }
+
+  tmp.colors = {
+    bg = colors.black,
+    fg = colors.white,
+    titlefg = colors.black,
+    titlebg = colors.white,
+    questionbg = colors.black,
+    questionfg = colors.white,
+    answerbg = colors.black,
+    answerfg = colors.gray
+    info
   }
 
   tmp.title = "Untitled"
