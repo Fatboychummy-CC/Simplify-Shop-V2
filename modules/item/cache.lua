11b050b27de0173e3c6fb1743f7bdc1bad0bf02d
Matthew W <fatboychummy@gmail.com>
2019-07-15 20:32:05 -0600

    add cachefile

---
+++ modules/item/cache.lua
@@ -0,0 +1,43 @@
+local funcs = {}
+local meta = {}
+local met = {}
+meta.__index = met
+met.__type = "cache"
+
+local cache = {}
+local saveLocation = "/cache.ic"
+
+local function save()
+  local h = io.open(saveLocation, 'w')
+  if h then
+    h:write(textutils.serialize(cache)):close()
+  else
+    error("Failed to open cache save file for writing.")
+  end
+end
+
+function funcs.addToCache(itemName, itemID, itemDamage)
+  if not cache[itemID] then cache[itemID] = {} end
+  cache[itemID][itemDamage] = itemName
+  save()
+end
+
+function funcs.removeFromCache(itemID, itemDamage)
+  if cache[itemID] then
+    cache[itemID][itemDamage] = nil
+  end
+  save()
+end
+
+function getRegistration(itemID, itemDamage)
+  if cache[itemID] then
+    return cache[itemID][itemDamage]
+  end
+  return false
+end
+
+function setSaveLocation(location)
+  saveLocation = location
+end
+
+return funcs
