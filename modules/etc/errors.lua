1390d4a61d54aa72f20ffea06c36a1c7ba841bf7
Matthew W <fatboychummy@gmail.com>
2019-07-15 19:11:11 -0600

    added watch

--- modules/etc/errors.lua
+++ modules/etc/errors.lua
@@ -5,4 +5,9 @@ function funcs.create(arg, expect, actual, level)
          .. ", got " .. type(actual), level or 2
 end
 
+function funcs.watch(arg, expect, actual, level)
+  return type(actual) == expect and actual
+    or error(funcs.create(arg, expect, actual, level and level + 1 or 3))
+end
+
 return funcs
