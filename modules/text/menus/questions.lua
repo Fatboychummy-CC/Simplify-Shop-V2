aa442d2f9ba0897a69d6e98833f54d81ea6f8182
Matthew W <fatboychummy@gmail.com>
2019-07-15 20:11:55 -0600

    questions seem to work properly.

--- modules/text/menus/questions.lua
+++ modules/text/menus/questions.lua
@@ -8,19 +8,27 @@ local e = require(".simplify.modules.etc.errors")
 local ew = e.watch
 local ec = e.create
 
-local function ask(self, question, tp)
+local function ask(self, questionIndex, tp)
   ew(1, "table", self)
   if self.__type ~= "menuObject" then
     error(ec(1, "menuObject", self))
   end
-  ew(2, "string", question)
+  ew(2, "number", questionIndex)
   ew(3, "string", tp)
+
+  local question = self.questions.q[questionIndex]
+  local info = self.questions.i[questionIndex]
+  local mx, my = term.getSize()
 
   term.setBackgroundColor(self.colors.questionbg)
   term.setTextColor(self.colors.questionfg)
   term.clear()
   term.setCursorPos(1, 1)
   print(question)
+  term.setCursorPos(1, 7)
+  term.setBackgroundColor(self.colors.infobg)
+  term.setTextColor(self.colors.infofg)
+  print(info)
   term.setCursorPos(1, 5)
   io.write("> ")
   term.setBackgroundColor(self.colors.answerbg)
@@ -29,6 +37,24 @@ local function ask(self, question, tp)
   if tp == "string" then
     return io.read()
   elseif tp == "number" then
+    while true do
+      local ans = tonumber(io.read())
+      if not ans then
+        term.setCursorPos(1, 5)
+        io.write(">" .. string.rep(' ', mx - 1))
+        term.setCursorPos(3, 5)
+        term.setTextColor(term.isColor() and colors.red or colors.gray)
+        io.write("Not a number!")
+        os.sleep(2)
+        term.setTextColor(self.colors.infofg)
+        term.setCursorPos(1, 5)
+        io.write(">" .. string.rep(' ', mx - 1))
+        term.setCursorPos(3, 5)
+        term.setTextColor(self.colors.answerfg)
+      else
+        return ans
+      end
+    end
     return tonumber(io.read()) or 0
   elseif tp == "boolean" then
     io.write(" false  true")
@@ -69,7 +95,7 @@ end
 
 function met:go()
   for i, question in ipairs(self.questions.q) do
-    self.questions.a[i] = ask(self, question, self.questions.t[i])
+    self.questions.a[i] = ask(self, i, self.questions.t[i])
   end
 end
 
@@ -92,7 +118,7 @@ function funcs.new()
     questionbg = colors.black,
     questionfg = colors.white,
     answerbg = colors.black,
-    answerfg = colors.gray,
+    answerfg = colors.white,
     infobg = colors.black,
     infofg = colors.lightGray
   }
