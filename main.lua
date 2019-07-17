9ed2e2a8bf3e4c3ddd46e19d7ed32568b42233e9
Matthew W <fatboychummy@gmail.com>
2019-07-16 19:44:59 -0600

    finished the item editor

--- main.lua
+++ main.lua
@@ -242,43 +242,83 @@ local function removeItem()
 
 end
 
-local function editItem()
-  local c = cache.getCache()
-
-  local menu = smenus.newMenu()
-  menu.title = "Edit Item Data"
-  menu.info = "Change values of items in your shop."
-
-  local registry = {}
-
-  for key, reg in pairs(c) do
-    for damage, registration in pairs(reg) do
-      local sName = registration.name
-      if #sName > 12 then
-        sName = sName:sub(1, 9) .. "..."
-      end
-      menu:addMenuItem(
-        sName,
-        "Edit this item.",
-        "Edit the item " .. key .. "[" .. tostring(damage) .. "]"
-      )
-      registry[#registry + 1] = {key = key, damage = damage}
-    end
-  end
+local function cacheEdit(c, registry)
+  local menu = imenus.newMenu()
+  menu.title = "Edit item."
+  menu.info = registry.key .. " with damage " .. tostring(registry.damage)
+              .. "."
 
   menu:addMenuItem(
-    "Return",
-    "Go back.",
-    "Return to the previous page."
+    "Display Name",
+    "string",
+    c[registry.key][registry.damage].name,
+    "The name to be displayed for this item."
+  )
+  menu:addMenuItem(
+    "Value",
+    "number",
+    c[registry.key][registry.damage].value,
+    "The value of this item (krist per item)"
+  )
+  menu:addMenuItem(
+    "Enabled",
+    "boolean",
+    c[registry.key][registry.damage].enabled,
+    "If disabled, the shop will not display this item, but it will not be deleted."
   )
 
+  menu:go()
+  if menu.menuItems.appends[3] then
+    menu.menuItems.appends[3] = true
+  else
+    menu.menuItems.appends[3] = false
+  end
+
+  cache.addToCache(
+    menu.menuItems.appends[1],
+    registry.key,
+    registry.damage,
+    menu.menuItems.appends[2],
+    menu.menuItems.appends[3]
+  )
+end
+
+local function editItem()
   while true do
+    local c = cache.getCache()
+
+    local menu = smenus.newMenu()
+    menu.title = "Edit Item Data"
+    menu.info = "Change values of items in your shop."
+
+    local registry = {}
+
+    for key, reg in pairs(c) do
+      for damage, registration in pairs(reg) do
+        local sName = registration.name
+        if #sName > 12 then
+          sName = sName:sub(1, 9) .. "..."
+        end
+        menu:addMenuItem(
+          sName,
+          "Edit this item.",
+          "Edit the item " .. key .. "[" .. tostring(damage) .. "]"
+        )
+        registry[#registry + 1] = {key = key, damage = damage}
+      end
+    end
+
+    menu:addMenuItem(
+      "Return",
+      "Go back.",
+      "Return to the previous page."
+    )
+
     local ans = menu:go()
     if ans == #registry + 1 then
       return
     else
-      print("nope")
-      os.sleep(1)
+      cacheEdit(c, registry[ans])
     end
   end
 end
