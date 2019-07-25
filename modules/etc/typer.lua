ce48f267b61e00fff8ebb9d4b15b2b1f96c290c5
Matthew W <fatboychummy@gmail.com>
2019-07-25 01:24:26 -0600

    yeet

--- modules/etc/typer.lua
+++ modules/etc/typer.lua
@@ -1,13 +1,36 @@
 local oldType = _G.type
 
-_G.type = function(input)
+local typeCheck = getmetatable(oldType)
+if typeCheck then
+  return
+end
+-- the above checks if there is a metatable for _G.type.  If there is, we have
+-- already changed what _G.type is.
+
+local function typer(_, input)
   local ok, a = pcall(getmetatable, input)
-  if ok and oldType(input) == "table" and oldType(a) == "table" then
+  local oldInType = oldType(input)
+
+  if ok and oldInType == "table" and type(a) == "table" then
+    if a.__masterType then
+      return a.__masterType
+    end
     if a.__type then
       return "userdata", a.__type
+    end
+    if oldInType == "string" then
+      return "string"
     end
     return "table"
   end
 
   return oldType(input)
 end
+
+_G.type = setmetatable(
+  {},
+  {
+    __call = typer,
+    __masterType = "function"
+  }
+)
