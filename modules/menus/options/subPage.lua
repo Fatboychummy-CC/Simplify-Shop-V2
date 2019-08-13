28c97dc3c8db0ceefd3cd064694ba94221552b13
Matthew W <fatboychummy@gmail.com>
2019-08-13 14:25:48 -0600

    add sub-page system to the options menu.

---
+++ modules/menus/options/subPage.lua
@@ -0,0 +1,94 @@
+local imenus = require("modules.menus.layouts.insert")
+
+local function subPage(page, settingsLocation, notify)
+  local menu = imenus.newMenu()
+  local sets = {}
+
+  ----------------------------------------------------------
+  -- func:    updater
+  -- inputs:  none
+  -- returns: nil
+  -- info:    to be run by the menu when a menu option is updated.
+  --          Notifies other modules of a settings update.
+  ----------------------------------------------------------
+  local function updater()
+    -- for each setting, set them to the new menu option
+    for i = 1, #sets do
+      settings.set(sets[i], menu.menuItems.appends[i])
+    end
+    settings.save(settingsLocation)
+    notify("settings_update") -- notify all modules
+
+    -- sets the menu options back to the settings.
+    -- since the settings may be changed after notifying.
+    for i = 1, #sets do
+      local append = menu.menuItems.appends[i]
+      if settings.get(sets[i]) ~= append then
+        menu.menuItems.appends[i] = settings.get(sets[i])
+      end
+    end
+  end
+
+  --[[
+  {
+    name = "Monitor",
+    info = "Change monitor settings.",
+    bigInfo = "Change things such as text-display size, and which monitor "
+              .. "to display on.",
+    settings = {
+      [ "shop.monitor.monitor" ] = {
+        "Monitor",
+        "type"
+        "The name of the monitor on the wired network."
+      },
+      [ "shop.monitor.textScale" ] = {
+        "Text Scale",
+        "type"
+        "The scale of the text for the monitor. Min 0.5, max 4."
+      }
+    }
+  }
+  ]]
+
+  local function getSetting(tp, name)
+    local set = settings.get(name)
+
+    if tp == "string" then
+      if type(set) == "string" then
+        return set
+      else
+        return "ERROR_5"
+      end
+    elseif tp == "number" then
+      if type(set) == "number" then
+        return set
+      else
+        return 0
+      end
+    elseif tp == "boolean" then
+      if type(set) == "boolean" then
+        return set
+      else
+        return 'a'
+      end
+    end
+  end
+
+  for i, setting in pairs(page.settings) do
+    sets[#sets + 1] = setting[1]
+    menu:addMenuItem(
+      setting[2],
+      setting[3],
+      getSetting(setting[3], setting[1]),
+      setting[4]
+    )
+  end
+
+  menu:go(updater)
+
+  updater()
+
+
+end
+
+return subPage
--- modules/menus/optionsMenu.lua
+++ modules/menus/optionsMenu.lua
@@ -1,4 +1,6 @@
-local imenus = require("modules.menus.layouts.insert")
+local smenus = require("modules.menus.layouts.simple")
+
+local sub = require("modules.menus.options.subPage")
 
 --TODO: SPLIT THIS INTO MULTIPLE MENUS
 
@@ -9,116 +11,126 @@ local imenus = require("modules.menus.layouts.insert")
 -- info:    runs the menu where you can change settings.
 ----------------------------------------------------------
 local function optionsMenu(sets, settingsLocation, notify)
-  local menu = imenus.newMenu()
-  menu.title = "Settings"
-  menu.info = "Select an item to edit it's value."
+  local menu = smenus.newMenu()
+  menu.title = "Options"
+  menu.info = "Select a sub-page."
 
-  menu:addMenuItem(
-    "Shop name",
-    "string",
-    settings.get("shop.shopName") or "ERROR 1",
-    "The name to be displayed for the shop."
-  )
-  menu:addMenuItem(
-    "Shop owner",
-    "string",
-    settings.get("shop.shopOwner") or "ERROR 1",
-    "Who owns this shop?"
-  )
-  menu:addMenuItem(
-    "Refresh rate",
-    "number",
-    settings.get("shop.refreshRate") or -1,
-    "Speed at which the shop will refresh it's screen (in seconds)."
-  )
-  menu:addMenuItem(
-    "Data folder",
-    "string",
-    settings.get("shop.dataLocation") or "ERROR 1",
-    "File system location at which the data folder will be stored"
-  )
-  menu:addMenuItem(
-    "Cache Name",
-    "string",
-    settings.get("shop.cacheSaveName") or "ERROR 1",
-    "Location at which the cache will be saved."
-  )
-  menu:addMenuItem(
-    "Log folder",
-    "string",
-    settings.get("shop.logLocation") or "ERROR 1",
-    "Location at which the logs folder will be saved."
-  )
-  menu:addMenuItem(
-    "Error Timer",
-    "number",
-    settings.get("shop.rebootTime") or "ERROR 1",
-    "When an error occurs, the shop will wait this time (in seconds) to reboot."
-  )
-  local autorun = settings.get("shop.autorun")
-  if type(autorun) == "boolean" then
+  local subPages = {
+    {
+      name = "Shop Info",
+      info = "Change shop info.",
+      bigInfo = "Change information like the name of your shop and etc.",
+      settings = {
+        {
+          "shop.shopName",
+          "Shop name",
+          "string",
+          "The name to be displayed for the shop."
+        },
+        {
+          "shop.shopOwner",
+          "Shop owner",
+          "string",
+          "Who owns this shop?"
+        },
+        {
+          "shop.refreshRate",
+          "Refresh rate",
+          "number",
+          "Rate at which the shop will refresh it's screen (in seconds)."
+        },
+        {
+          "shop.autorun",
+          "Autorun",
+          "boolean",
+          "Should the shop autorun on boot?"
+        },
+        {
+          "shop.autorunTime",
+          "Autorun time",
+          "number",
+          "How long should the shop wait until being run if autorun is enabled?"
+        },
+        {
+          "shop.rebootTime",
+          "Error timer",
+          "number",
+          "When an error occurs, the shop will wait this time (in seconds) to "
+          .. "reboot."
+        }
+      }
+    },
+    {
+      name = "Data",
+      info = "Change data settings.",
+      bigInfo = "Change the locations of save files.",
+      settings = {
+        {
+          "shop.dataLocation",
+          "Data folder",
+          "string",
+          "File system location at which the data folder will be stored."
+        },
+        {
+          "shop.cacheSaveName",
+          "Cache File",
+          "string",
+          "File system location at which the cache will be saved."
+        },
+        {
+          "shop.logLocation",
+          "Log folder",
+          "string",
+          "File system location at which the logs folder will be saved."
+        }
+      }
+    },
+    {
+      name = "Monitor",
+      info = "Change monitor settings.",
+      bigInfo = "Change things such as text-display size, and which monitor "
+                .. "to display on.",
+      settings = {
+        {
+          "shop.monitor.monitor",
+          "Monitor",
+          "string",
+          "The name of the monitor on the wired network."
+        },
+        {
+          "shop.monitor.textScale",
+          "Text Scale",
+          "number",
+          "The scale of the text for the monitor. Min 0.5, max 4."
+        }
+      }
+    }
+  }
+
+  for i, subPage in ipairs(subPages) do
     menu:addMenuItem(
-      "Autorun",
-      "boolean",
-      autorun,
-      "Should the shop autorun on boot?"
-    )
-  else
-    menu:addMenuItem(
-      "Autorun",
-      "boolean",
-      true,
-      "Should the shop autorun on boot?"
+      subPage.name,
+      subPage.info,
+      subPage.bigInfo
     )
   end
   menu:addMenuItem(
-    "Autorun Time",
-    "number",
-    settings.get("shop.autorunTime") or "ERROR 1",
-    "How long should the shop wait until being run if autorun enabled?"
-  )
-  menu:addMenuItem(
-    "Monitor",
-    "string",
-    settings.get("shop.monitor.monitor") or peripheral.findString("monitor")[1]
-      or "NO MONITOR",
-    "The name of the monitor on the wired network."
-  )
-  menu:addMenuItem(
-    "Text Size",
-    "number",
-    settings.get("shop.monitor.textScale") or 0.5,
-    "The scale of the text for the monitor.  Min 0.5, max 4"
+    "Return",
+    "Go back.",
+    "Return to the startup page."
   )
 
-  ----------------------------------------------------------
-  -- func:    updater
-  -- inputs:  none
-  -- returns: nil
-  -- info:    to be run by the menu when a menu option is updated.
-  --          Notifies other modules of a settings update.
-  ----------------------------------------------------------
-  local function updater()
-    -- for each setting, set them to the new menu option
-    for i = 1, #sets do
-      settings.set(sets[i], menu.menuItems.appends[i])
-    end
-    settings.save(settingsLocation)
-    notify("settings_update") -- notify all modules
+  while true do
+    local ans = menu:go()
 
-    -- sets the menu options back to the settings.
-    -- since the settings may be changed after notifying.
-    for i = 1, #sets do
-      local append = menu.menuItems.appends[i]
-      if settings.get(sets[i]) ~= append then
-        menu.menuItems.appends[i] = settings.get(sets[i])
-      end
+    if ans >= 1 and ans < menu:count() then
+      sub(subPages[ans], settingsLocation, notify)
+    else
+      return
     end
   end
 
-  menu:go(updater)
-
-  updater()
+  --updater()
 end
 
 return optionsMenu
