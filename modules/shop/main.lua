289f645b6673cadda40e9ec9e1fed5186a75f2d8
Matthew W <fatboychummy@gmail.com>
2019-08-18 03:10:18 -0600

    draw the list and check for settings updates.

--- modules/shop/main.lua
+++ modules/shop/main.lua
@@ -1,5 +1,6 @@
 local funcs = {}
 local moni = require("modules.etc.monitor")
+local l = require("modules.shop.monitor.listy")
 
 local onotify
 
@@ -8,6 +9,20 @@ function funcs.notify(ev, ...)
 
   if ev == "init" then
     onotify = args["notify"]
+  elseif ev == "settings_update" then
+    local set = args[1]
+    local t = settings.get(set)
+    if set == "shop.listing.topStop" or set == "shop.listing.leftStop" then
+      if t < 1 then
+        settings.set(set, 1)
+      end
+    elseif set == "shop.listing.rightStop" then
+      local mx = peripheral.call(settings.get("shop.monitor.monitor"),
+                                 "getSize")
+      if t > mx then
+        settings.set(set, mx)
+      end
+    end
   end
 end
 
@@ -15,10 +30,18 @@ function funcs.go()
   local mon = peripheral.wrap(settings.get("shop.monitor.monitor"))
   moni.setupMonitor(mon, true)
 
+  local top = settings.get("shop.listing.topStop")
+  local lef = settings.get("shop.listing.leftStop")
+  local rig = settings.get("shop.listing.rightStop")
+  local mx = settings.get("shop.listing.maxItemsPerPage")
+  local dcml = settings.get("shop.listing.decimalPlaces")
+
+  local list = l.createList(lef, top, rig, top + mx)
   while true do
+    mon.setBackgroundColor(colors.black)
     mon.clear()
-    mon.setCursorPos(1, 1)
-    mon.write("Yeah we runnin boiiiiiiiiiiis")
+    list:draw(mon, dcml)
+    mon.flush()
     os.sleep(5)
   end
 end
